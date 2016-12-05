8169fe7be61ecb968f10d057eb66c16b29909bc7
Sajel <sajel15@gmail.com>
2016-12-05 21:37:29 +0200

    Prediction test.

--- src/main/java/org/dimamir999/model/Prediction.java
+++ src/main/java/org/dimamir999/model/Prediction.java
@@ -8,6 +8,20 @@ public class Prediction {
     }
 
     public Prediction(double probabilityFirstWin) {
+        if(probabilityFirstWin < 0) {
+            throw new IllegalArgumentException("Negative probability.");
+        }
+        if (probabilityFirstWin > 1) {
+            throw new IllegalArgumentException("Probability is above 1.");
+        }
         this.probabilityFirstWin = probabilityFirstWin;
+    }
+
+    public double getProbabilityFirstWin() {
+        return  probabilityFirstWin;
+    }
+
+    public double getProbabilitySecondWin() {
+        return 1 - probabilityFirstWin;
     }
 }
