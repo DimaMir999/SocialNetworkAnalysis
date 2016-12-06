1b34591052ea6703f966f830c8583ef6fdb2c86e
DimaMir999 <dimamir999@yandex.ru>
2016-12-06 11:39:48 +0200

    Add json answer from python code and fix parsing json from input.

--- src/main/resources/predict.py
+++ src/main/resources/predict.py
@@ -6,12 +6,15 @@ model = joblib.load('model.pkl')
 
 while True:
     match_json = input()
-    match_info = json.load(match_json)
+    match_info = json.loads(match_json)
     features = []
     for feature_name in order:
         features.append(match_info[feature_name])
 
     probability = model.predict_proba([features])
 
+    json.dumps({
+        'winFirstProbability': probability[0][1],
+        'winSecondProbability': probability[0][0]
+    })
     print(probability[0])
-
